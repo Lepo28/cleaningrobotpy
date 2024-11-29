4e70ba59def695cc30389438c000a51ed1dfea73
Leonardo Porcelli <61625009+Lepo28@users.noreply.github.com>
2024-11-29 18:42:59 +0100

    Green Phase of US 4 (2)

--- src/cleaning_robot.py
+++ src/cleaning_robot.py
@@ -73,6 +73,15 @@ class CleaningRobot:
         return f"({self.pos_x},{self.pos_y},{self.heading})"
 
     def execute_command(self, command: str) -> str:
+
+        obstacle_found = self.obstacle_found()
+
+        if obstacle_found:
+            if command == self.FORWARD:
+                obstacle_x = self.pos_x - 1 if self.heading == self.W else self.pos_x + 1 if self.heading == self.E else self.pos_x
+                obstacle_y = self.pos_y + 1 if self.heading == self.N else self.pos_y - 1 if self.heading == self.S else self.pos_y
+                return self.robot_status() + f'({obstacle_x},{obstacle_y})'
+
         if command == self.FORWARD:
             self.activate_wheel_motor()
             self.pos_y += 1
